820ed7d2e6b2a5b0369f2476b30e9ab82b2fd89d
ismailguven91 <91503441+ismailguven91@users.noreply.github.com>
2023-05-09 11:38:53 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,81 @@
+const selectionButtons = document.querySelectorAll('[data-selection]')
+const finalColumn = document.querySelector('[data-final-column]')
+const computerScoreSpan = document.querySelector('[data-computer-score]')
+const yourScoreSpan = document.querySelector('[data-your-score]')
+const SELECTIONS = [
+  {
+    name: 'rock',
+    emoji: '✊',
+    beats: 'scissors'
+  },
+  {
+    name: 'paper',
+    emoji: '✋',
+    beats: 'rock'
+  },
+  {
+    name: 'scissors',
+    emoji: '✌',
+    beats: 'paper'
+  }
+]
+
+selectionButtons.forEach(selectionButton => {
+  selectionButton.addEventListener('click', e => {
+    const selectionName = selectionButton.dataset.selection
+    const selection = SELECTIONS.find(selection => selection.name === selectionName)
+    makeSelection(selection)
+  })
+})
+
+function makeSelection(selection) {
+  const computerSelection = randomSelection()
+  const yourWinner = isWinner(selection, computerSelection)
+  const computerWinner = isWinner(computerSelection, selection)
+
+  addSelectionResult(computerSelection, computerWinner)
+  addSelectionResult(selection, yourWinner)
+
+  if (yourWinner) incrementScore(yourScoreSpan)
+  if (computerWinner) incrementScore(computerScoreSpan)
+}
+
+function incrementScore(scoreSpan) {
+  scoreSpan.innerText = parseInt(scoreSpan.innerText) + 1
+  if(parseInt(yourScoreSpan.innerText) == 3){
+    alert(`You won:` + " " + localStorage.getItem(`textvalue`) + ": " + yourScoreSpan.innerText + " - Computer: " + computerScoreSpan.innerText);
+    window.location = `index.html`;
+}
+else if(parseInt(computerScoreSpan.innerText) == 3){
+    alert(`Computer wins: ` + localStorage.getItem(`textvalue`) + ": " + yourScoreSpan.innerText + " -  Computer: " + computerScoreSpan.innerText)
+    window.location = `index.html`;
+}
+  
+}
+
+function addSelectionResult(selection, winner) {
+  const div = document.createElement('div')
+  div.innerText = selection.emoji
+  div.classList.add('result-selection')
+  if (winner) div.classList.add('winner')
+  finalColumn.after(div)
+}
+
+function isWinner(selection, opponentSelection) {
+  return selection.beats === opponentSelection.name
+}
+
+function randomSelection() {
+  const randomIndex = Math.floor(Math.random() * SELECTIONS.length)
+  return SELECTIONS[randomIndex]
+}
+
+
+function passvalue(){
+    var myname = document.getElementById("myname").value;
+    localStorage.setItem("textvalue", myname);
+    return false;
+}
+
+
+document.getElementById("result").innerHTML=localStorage.getItem("textvalue");
